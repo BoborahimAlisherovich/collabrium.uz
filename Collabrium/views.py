5aec9a5a851965ecdbf1989907343a6c35decf54
khusanoof <khusanovmuhammadjon335@gmail.com>
2024-12-12 20:52:53 +0500

    collabrium

--- Collabrium/views.py
+++ Collabrium/views.py
@@ -4,6 +4,114 @@ from django.shortcuts import render
 from rest_framework import viewsets,permissions
 from .serializers import RezidentSerializer,OurTeamSerializer
 from .models import Rezident
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from .models import Space,Faq
+from .serializers import SpaceSerializer,FaqSerializer
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Collabrium",
+        default_version='v1',
+        description="Space and Faq",
+        contact=openapi.Contact(email="collabrium@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+class SpaceListCreateView(APIView):
+    def get(self, request):
+        spaces = Space.objects.all()
+        serializer = SpaceSerializer(spaces, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = SpaceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SpaceDetailView(APIView):
+    def get(self, request, slug):
+        try:
+            space = Space.objects.get(page_slug=slug)
+            serializer = SpaceSerializer(space)
+            return Response(serializer.data)
+        except Space.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, slug):
+        try:
+            space = Space.objects.get(page_slug=slug)
+            serializer = SpaceSerializer(space, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Space.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, slug):
+        try:
+            space = Space.objects.get(page_slug=slug)
+            space.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Space.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+class FaqListCreateView(APIView):
+    def get(self, request):
+        faqs = Faq.objects.all()
+        serializer = FaqSerializer(faqs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FaqSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FaqDetailView(APIView):
+    def get(self, request, slug):
+        try:
+            faq = Faq.objects.get(page_slug=slug)
+            serializer = FaqSerializer(faq)
+            return Response(serializer.data)
+        except Faq.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, slug):
+        try:
+            faq = Faq.objects.get(page_slug=slug)
+            serializer = FaqSerializer(faq, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Faq.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, slug):
+        try:
+            faq = Faq.objects.get(page_slug=slug)
+            faq.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Faq.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
 
 
 class OurTeamSerializerViewSet(viewsets.ModelViewSet):
--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("api/Rezident/",RezidentSerializerViewSet.as_view({'get':'list'}),name = "rezident")
+    path("api/Rezident/",RezidentSerializerViewSet.as_view({'get':'list'}),name = "rezident"),
 
 ]
