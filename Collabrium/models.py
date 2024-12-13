955d5add7ff67d6f3f93f1e8fce5ad6b7268ac59
Boborahim <boborahimrustamqulov0@gmail.com>
2024-12-13 20:33:18 +0500

    verbose_name

--- Collabrium/models.py
+++ Collabrium/models.py
@@ -8,8 +8,8 @@ class Space(models.Model):
     page_slug = models.SlugField(unique=True) 
     image = models.ImageField(upload_to='Images/space')
     class Meta:
-        verbose_name = _("Space")
-        verbose_name_plural = _("Spaces")
+        verbose_name = _("пространства")
+        verbose_name_plural = _("пространства")
 
     def __str__(self):
         return f"{self.space}"
@@ -22,8 +22,8 @@ class Faq(models.Model):
     page_slug = models.SlugField()
 
     class Meta:
-        verbose_name = _("Faq")
-        verbose_name_plural = _("Faqs")
+        verbose_name = _("Часто задаваемые вопросы")
+        verbose_name_plural = _("Часто задаваемые вопросыc")
     
     def __str__(self):
         return f"{self.title}"
@@ -34,10 +34,13 @@ class Blog(models.Model):
     image_cover = models.ImageField(upload_to='Blog_images',verbose_name="Обложка изображения")
     date = models.DateField(auto_now_add=True,verbose_name="Дата публикации")
     title = models.CharField(max_length=255,verbose_name="Заголовок статьи")
-    page_slug = models.SlugField(verbose_name="Слаг страницы")
+    page_slug = models.SlugField(unique=True,verbose_name="Слаг страницы")
 
     def __str__(self):
         return f"{self.title}"
+    class Meta:
+        verbose_name = _("Блог")
+        verbose_name_plural = _("Блог")
 
 
 
@@ -48,8 +51,8 @@ class OurTeam(models.Model):
     job = models.CharField(max_length=200,verbose_name="Работа")
     description = models.TextField(verbose_name="описание")
     class Meta:
-        verbose_name = _("OurTeam")
-        verbose_name_plural = _("OurTeams")
+        verbose_name = _("Наша команда")
+        verbose_name_plural = _("Наша командаc")
 
     def __str__(self):
         return f"{self.name}"
@@ -62,8 +65,8 @@ class Rezident(models.Model):
     job = models.CharField(max_length=200,verbose_name="Работа")
     description = models.TextField(verbose_name="описание")
     class Meta:
-        verbose_name = _("Rezident")
-        verbose_name_plural = _("Rezidents")
+        verbose_name = _("Резидент")
+        verbose_name_plural = _("Резидентc")
 
     def __str__(self):
         return f"{self.name}"
