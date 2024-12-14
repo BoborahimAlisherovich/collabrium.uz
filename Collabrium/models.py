240ca97a78a76faa43044eb06122513e186bb480
Boborahim <boborahimrustamqulov0@gmail.com>
2024-12-14 17:15:33 +0500

    Update

--- Collabrium/models.py
+++ Collabrium/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save,pre_save
+from django.core.exceptions import ValidationError
+from django.dispatch import receiver
 
-#1
+
 class Space(models.Model):
     space = models.CharField(max_length=300, verbose_name="места")
-    page_slug = models.SlugField(unique=True, verbose_name="Слаг страницы") 
+    page_slug = models.SlugField(unique=True, verbose_name="Слаг страницы")
     image = models.ImageField(upload_to='Images/space', verbose_name="изображение")
-
-    class Meta:
-        verbose_name = _("места")
-        verbose_name_plural = _("места")
+    is_potkast = models.BooleanField(default=False, verbose_name="Это подкаст")
 
     def __str__(self):
-        return f"{self.space}"
-    
+        return self.space
+
+    class Meta:
+        verbose_name = _("Место")
+        verbose_name_plural = _("Места")
+
 
 #2
 class Faq(models.Model):
@@ -78,10 +82,33 @@ class Rezident(models.Model):
         return f"{self.name}"
 
 
-
 class Podkast(models.Model):
-    total = models.CharField(max_length=200,verbose_name="инструмент")
-    image = models.ImageField(verbose_name="изображение")
+    total = models.CharField(max_length=200, verbose_name="инструмент")
+    image = models.ImageField(upload_to="Images/podkast", verbose_name="изображение")
+
+    def __str__(self):
+        return self.total
+
     class Meta:
         verbose_name = _("Подкаст")
-        verbose_name_plural = _("Подкаст")
+        verbose_name_plural = _("Подкасты")
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from .models import Space, Podkast
+
+@receiver(pre_save, sender=Space)
+def save_to_podkast_instead_of_space(sender, instance, **kwargs):
+    """
+    Agar is_potkast belgilangan bo'lsa:
+    1. Podkast modeliga ma'lumot saqlanadi.
+    2. Space modelini saqlash to'xtatiladi.
+    """
+    if instance.is_potkast:  # is_potkast True bo'lsa
+        # Podkast obyektini yaratamiz yoki mavjud bo'lsa qaytaramiz
+        Podkast.objects.get_or_create(
+            total=instance.space,
+            image=instance.image
+        )
+        # Space saqlanishini to'xtatamiz
